90be493a5545d10edf199916fb36a0248f76be3d
davalcato <davalcato@yahoo.com>
2021-08-01 12:48:23 -0700

    first commit

---
+++ ImageCoreML/ImageCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCoreMLApp.swift
+//  ImageCoreML
+//
+//  Created by Daval Cato on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
